e3ffc0f11e5dd5a6362eecadbbb5bfe61dfa9904
sanghita <sanghita@gmail.com>
2024-03-26 13:15:43 +0530

    base url changed

--- react-client/src/service/api.js
+++ react-client/src/service/api.js
@@ -1,5 +1,5 @@
 // Base API URL
-const baseApi="http://localhost:8000/api/v1"
+const baseApi="https://car-booking-website.onrender.com/api/v1"
 
 
 // Object containing endpoint paths
@@ -8,5 +8,6 @@ export const endpoints={
     GET_CARS:baseApi+"/cars/getAvailableCars",
     BOOK_CAR:baseApi+"/booking/bookCar",
     GET_ALL_BOOKING_DETAILS:baseApi+"/booking/getBookedCarDetails"
+
 
 }
